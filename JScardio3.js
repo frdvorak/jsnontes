643ce08a7dbffc5509837663e038746248ec7265
frdvorak <frantisek.dvorak011@gmail.com>
2018-11-18 18:20:06 +0000

    JS cardio 3

---
+++ JScardio3.js
@@ -0,0 +1,10 @@
+JS Cardio 3
+https://www.youtube.com/watch?v=tcoiPHktCwQ&t=290s
+
+// CHALLENGE 1: ADD ALL NUMBERS
+// Return a sum of all parameters regardless of the amount of numbers, no arrays
+
+// solution 1 - ES5 arguments & for loop
+function addAll(){
+	
+}
