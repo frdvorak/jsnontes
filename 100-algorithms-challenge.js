d668552d0b9e5caebafcdadc7b112ab26fe103c8
frdvorak <frantisek.dvorak011@gmail.com>
2018-11-20 22:27:02 +0000

    Remove duplicates from Array - order of array is NOT important

--- 100-algorithms-challenge.js
+++ 100-algorithms-challenge.js
@@ -1,4 +1,26 @@
-100 Algorithms Challenge
-https://www.udemy.com/100-algorithms-challenge/
+JS Algorithm Challenges
 
-//1
+// 1 - Remove duplicates from Array
+let a = [1,2,5,2,1,8];
+let b = [];
+let len = a.length; //so the system doesnt have to calculate it for every item in the for loop
+for (let i = 0; i<len; i++){
+	if(b.indexOf(a[i]) === -1){
+		b.push(a[i]);
+	}
+}
+console.log(b); //[1, 2, 5, 8]
+
+// 2 - Remove duplicates from Array - order of array is NOT important
+let a = [1,2,5,2,1,8];
+let b = [];
+let len = a.length;
+a.sort(); //[1, 1, 2, 2, 5, 8]
+let _temp; // here we will store the previous value
+for (let i=0; i<len; i++){
+	if(a[i] !== _temp){ 	// if current position in array !== _temp
+		b.push(a[i]);		// push it's value into b array
+		_temp = a[i];		// and set _temp to that value so we can check the next one
+	}
+}
+console.log(b); //[1, 2, 5, 8]
