0a267e007cfd6fd17ca8ea15ba396c14267bbfc3
frdvorak <frantisek.dvorak011@gmail.com>
2018-12-02 21:34:15 +0000

    16 Fibonacci

--- 100-algorithms-challenge.js
+++ 100-algorithms-challenge.js
@@ -219,3 +219,22 @@ console.log(
 	isUnique('abcdabcd'),		//false
 	isUnique('mnopgrSTUVWXyz!'),	//true
 );
+
+//16 Fibonacci
+function fibonacci(n){
+	const seq = [1,1];
+
+	if (n<2){
+		return seq.slice(0,n);
+	}
+	while (seq.length < n) {
+		const lastItem = seq[seq.length - 1];
+		const secondLastItem = seq[seq.length - 2];
+
+		seq.push(lastItem + secondLastItem);
+	}
+
+	return seq;
+
+}
+fibonacci(5); //(5) [1, 1, 2, 3, 5]
